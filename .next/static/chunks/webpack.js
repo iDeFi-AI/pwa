71b7851f1320ddba3e45f762ab026769dd107a28
root <root@b1h0>
2024-01-26 07:23:34 -0700

    fixed api env

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0c8dad2955574117"; }
+/******/ 		__webpack_require__.h = function() { return "0dfa9190296018bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/constants/env.ts
+++ src/constants/env.ts
@@ -2,6 +2,6 @@ export const isProd = process.env.NODE_ENV === 'production'
 export const isLocal = process.env.NODE_ENV === 'development'
 
 // constants/env.ts
-export const openaiApiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY || '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';
+export const openaiApiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY || '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';
 export const showLogger = process.env.NEXT_PUBLIC_SHOW_LOGGER === 'true' || false;
 
