575c048fdaa7dcd7d471427ee5886f8f4f9b9d48
root <root@b1h0>
2024-01-26 06:51:47 -0700

    fixed api env

--- .next/static/chunks/app/layout-df9183cb6d0c9f98.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5253:function(n,e,u){Promise.resolve().then(u.t.bind(u,6579,23))},6579:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=5253)}),_N_E=n.O()}]);
