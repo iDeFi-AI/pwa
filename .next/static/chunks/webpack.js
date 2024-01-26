a2657dc50db134b9778ca78e58b1c4636bb27a65
root <root@b1h0>
2024-01-26 06:58:51 -0700

    fixed api env

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "43ba6213c72b2272"; }
+/******/ 		__webpack_require__.h = function() { return "0c8dad2955574117"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
